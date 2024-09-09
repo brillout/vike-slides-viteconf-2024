2034fa00ffc89d71ec42e052655476a36f19e9c4
Romuald Brillout <git@brillout.com>
2024-09-09 14:56:24 +0200

    wip

--- pages/1/+Page.tsx
+++ pages/1/+Page.tsx
@@ -1,34 +1,41 @@
-import { Logo } from "../../components/Logo";
-
 export { Page };
+
+import vikeIcon from '../../assets/vike-vertical.svg'
 
 function Page() {
   return (
-    <div>
-      <Center>
-        <Logo size={120} style={{marginRight: 30}} />
-    {' '}
-        <code style={{
-          fontSize: '3rem',
-            padding: '10px 17px',
-            fontWeight: 500,
-            borderRadius: 7,
-        }}>vite-plugin-ssr</code>
-      </Center>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontSize: 16 }}>
+      <div
+        style={{
+          display: 'flex',
+          height: 320,
+          /*
+          scale: '1.2'
+          */
+        }}
+      >
+        <img src={vikeIcon} style={{ marginRight: 50, marginTop: 0, height: '100%' }} />
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', height: '100%' }}>
+          <div>
+            <span
+              style={{
+                fontSize: '8.8em',
+                display: 'inline-block',
+                verticalAlign: 'middle',
+                fontWeight: 500,
+                lineHeight: '1.1em'
+              }}
+            >
+              Vike
+            </span>
+          </div>
+          <div style={{ fontSize: '4.2em', paddingLeft: 4, paddingTop: 0, paddingBottom: 0, lineHeight: '1.2em' }}>
+            Next Generation
+            <br />
+            Frontend Framework
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
-
-function Center({ style, ...props }: any) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        ...style,
-      }}
-      {...props}
-    ></div>
-  );
+  )
 }
